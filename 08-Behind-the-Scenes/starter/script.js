09cd89792b5d24f1e1340ac8ee7b5a62f8168c84
Grant Emerson <grant.alex.emerson@gmail.com>
2020-11-12 09:34:45 -0600

    video 98

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -33,35 +33,112 @@
 
 
 
-console.log(me);
+// console.log(me);
 // console.log(job);
 // console.log(year);
 
-var me = 'Grant';
-let job = 'teacher';
-const year = 1984;
+// var me = 'Grant';
+// let job = 'teacher';
+// const year = 1984;
 
 // Functions 
 // console.log(addDecl(2,3));
 // console.log(addExpr(2,3));
 // console.log(addArrow(2,3));
 
-function addDecl(a,b) {
-    return a + b;
-}
+// function addDecl(a,b) {
+//     return a + b;
+// }
 
+// const addExpr = function(a,b) {
+//     return a + b
+// }
+
+// var addArrow = (a, b) => a + b;
+
+// // Example
+// if(!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//     console.log('All products deleted');
+// }
+
+// console.log(this)
+
+// const calcAge = function(birthYear) {
+//     console.log(2037 - birthYear);
+//     // console.log(this);
+// }
+// calcAge(1984);
+
+// const calcAgeArrow = (birthYear) => {
+//     console.log(2037 - birthYear);
+//     // console.log(this);
+// }
+// calcAgeArrow(1991);
+
+// const grant = {
+//     year: 1991,
+//     calcAge: function() {
+//         console.log(this);
+//         console.log(2037 - this.year);
+//     }
+// }
+// grant.calcAge();
+
+// const matilda = {
+//     year: 2017,
+// };
+
+// matilda.calcAge = grant.calcAge;
+// matilda.calcAge()
+
+// const f = grant.calcAge;
+// f();
+
+// var firstName = 'Jonas'
+
+const grant = {
+    firstName: 'Grant',
+    year: 1991,
+    calcAge: function() {
+        // console.log(this);
+        console.log(2037 - this.year);
+
+        // const self = this;
+        // const isMillenial = function() {
+        //     console.log(self);
+        //     // console.log(this.year >= 1981 && this.year <= 1996);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        // };
+
+
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        };
+       isMillenial();
+    },
+    greet: () =>{
+        console.log(this);
+        console.log(`Hey ${this.firstName}`)},
+};
+
+grant.greet();
+grant.calcAge();
+
+// arguments keyword
 const addExpr = function(a,b) {
+    console.log(arguments);
     return a + b
 }
+addExpr(2,5);
+addExpr(2, 5, 8, 12)
 
-var addArrow = (a, b) => a + b;
-
-// Example
-if(!numProducts) deleteShoppingCart();
-
-var numProducts = 10;
-
-function deleteShoppingCart() {
-    console.log('All products deleted');
-}
-
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b
+};
+addArrow((2,5,8))
