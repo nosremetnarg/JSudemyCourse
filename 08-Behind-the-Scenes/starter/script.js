360360711db04d0351cf1bd5199a780a50fd9ea3
Grant Emerson <grant.alex.emerson@gmail.com>
2020-11-17 16:18:30 -0600

    challenge 3 obj redo

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,51 +1,223 @@
 // 'use strict';
 
-// const weekDays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun']
-// const openingHours = {
-//   [weekDays[3]]: {
-//     open: 12,
-//     close: 22,
-//   },
-//   [weekDays[4]]: {
-//     open: 11,
-//     close: 23,
-//   },
-//   [weekDays[5]]: {
-//     open: 0, // Open 24 hours
-//     close: 24,
-//   },
-// };
+const weekDays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekDays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-//   // ES6 enhanced obj literal
-//   openingHours,
+  // ES6 enhanced obj literal
+  openingHours,
 
-//   order(starterIndex, mainIndex) {
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-//   },
-//   orderDelivery({
-//     starterIndex = 1,
-//     mainIndex = 0,
-//     time = '20:00',
-//     address,
-//   }) {},
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {},
 
-//   orderPasta(ing1, ing2, ing3) {
-//     console.log(
-//       `here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
-//     );
-//   },
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
 
-//   orderPizza(mainIngredient, ...otherIngredients) {
-//     console.log(mainIngredient, otherIngredients);
-//   },
-// };
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
+};
+
+// coding challenge 3 =============================================
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// Attempt number two
+// 1. 
+
+const events = new Set(gameEvents.values());
+console.log(events);
+const keys = new Set(gameEvents.keys());
+console.log(keys);
+
+// 2. 
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
+
+// 4 .
+
+for (const [min, event] of gameEvents){
+  const half = min < 45 ? 'FIRST' : 'SECOND';
+  console.log(`${half} HALF: ${min} ${event}`);
+}
+
+
+// for (const [min, event] of gameEvents) {
+//   const half = min < 45 ? 'FIRST' : 'SECOND'
+//   console.log(`[${half} HALF]:${min} ${event}`);
+// }
+
+
+// // 1.
+// const eventSet = [...new Set(gameEvents.values())];
+// console.log(eventSet);
+
+// // 2. 
+// console.log(gameEvents.delete(64));
+// console.log(gameEvents);
+
+// // 3. 
+// console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+// const time = [...gameEvents.keys()].pop();
+// console.log(time);
+// console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
+
+
+
+// // 4.
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? "FIRST" : "SECOND"
+//   console.log(`[ ${half} HALF] ${min}: ${event}`);
+// }
+
+// coding challenge 3 =============================================
+
+
+// video 118 data structure summary ===========================
+
+// video 117 MAP II ===========================================
+
+// const question = new Map([
+//   ['question', 'what is the best programming language in the world'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct 🏄🏼‍♂️'],
+//   [false, 'try again'],
+// ]);
+// console.log(question);
+
+// // Convert object to Map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours))
+// console.log(hoursMap);
+
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+
+// // const answer = Number(prompt('Your answer'))
+// const answer = 3;
+// console.log(answer);
+
+// console.log(question.get(question.get('correct') === answer));
+
+// // Convert map to array
+// console.log([...question]);
+// // console.log(question.entries());
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
+
+// video 116 MAP =================================
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open ;D')
+//   .set(false, 'We are closed ;(');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2)
+// // rest.clear()
+// const arr = [1,2]
+// rest.set(arr, 'Test')
+// rest.set(document.querySelector('h1'), 'Heading')
+// console.log(rest);
+// console.log(rest.size);
+// console.log(rest.get(arr))
+
+// video 115 SET ================================
+// const ordersSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
+// console.log(ordersSet);
+// console.log(new Set('Grant'));
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Garlic Bread');
+// ordersSet.delete('Risotto');
+// // ordersSet.clear();
+// console.log(ordersSet);
+
+// for(const order of ordersSet) console.log(order);
+
+// // Example
+// const staff = ['waiter', 'chef', 'waiter', 'manager', 'waiter', 'chef'];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(new Set(['waiter', 'chef', 'waiter', 'manager', 'waiter', 'chef']).size);
+// console.log(new Set('grantemerson').size);
+// ==================== video 115
+
 
 // // property names
 // const properties = Object.keys(openingHours);
@@ -70,7 +242,6 @@
 //   console.log(`On ${key} we open at ${open} and close at ${close}`);
 // }
 
-
 // if(restaurant.openingHours && restaurant.openingHours.mon) console.log
 // (restaurant.openingHours.mon.open);
 
@@ -94,7 +265,6 @@
 //   {name: "Grant", email: 'grant@gmail.com'}
 // ];
 // console.log(users[0]?.name ?? "User array empty");
-
 
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
@@ -341,14 +511,36 @@ const game = {
     team2: 6.5,
   },
 };
+// Challenge 2 redo ============================================
+// 1.
 
-const ordersSet
+// for(const [i, player] of game.scored.entries()) // this method loops over an array
+// console.log(`Goal ${i +1 }: ${player}`);
 
+// 2.
 
+// let average = 0
+// const values = Object.values(game.odds)
+// console.log(values);
+// for (const odd of values)
+// average += odd;
+// console.log(average);
+// average /= values.length;
+// console.log(average);
 
+// // 3.
+// const odds = Object.entries(game.odds);
+// console.log(odds);
+// for (const [team, odd] of odds){
+//   console.log(team);
+// const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`
+// console.log(`Odd of ${teamStr} ${odd}`);
+// }
+
+// Challenge 2 redo  end ============================================
 
 // ================== challenge 2 ======================
-// 1. 
+// 1.
 // for ( const score of game.scored) { // for of loop and obj navigation
 //   console.log(score);
 // }
@@ -356,20 +548,16 @@ const ordersSet
 // for (const [i, player] of game.scored.entries()) // entries to get index and get goal number
 // console.log(`Goal ${i + 1}: ${player}`);
 
-
 // // 2.
 // let avg = 0
-// const { team1, x: draw, team2 } = game.odds;
-// const values = Object.values(game.odds)
-// for (const odd of values) 
+// const values1 = Object.values(game.odds)
+// for (const odd of values1)
 //   avg += odd;
 //   console.log(avg);
-//   avg /= values.length; // divide by length of array
+//   avg /= values1.length; // divide by length of array
 //   console.log(avg);
 
-
-
-// // 3. 
+// // 3.
 
 // for(const [team, odd]of Object.entries(game.odds)){
 //   const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`
@@ -412,7 +600,6 @@ const ordersSet
 // const { team1, x: draw, team2 } = game.odds;
 // console.log(team1, draw, team2);
 
-
 // const printGoals = (...player) => {
 //   console.log(player);
 // }
@@ -422,10 +609,6 @@ const ordersSet
 
 // team1 > team2 && console.log(`team2 is winning`);
 // team1 < team2 && console.log(`team1 is winning`);
-
-
-
-
 
 // let players1 = [...game.players[0]]; // team 1 players
 // let players2 = [...game.players[1]]; // team 2 players
