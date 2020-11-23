d3babb2683b0d2a04a31567b4527b363cc4169f7
Grant Emerson <grant.alex.emerson@gmail.com>
2020-11-23 15:53:42 -0600

    up to video 136

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,23 +1,63 @@
 // 'use strict';
 
+// video 136 ========== closures part 2 ===================
+
+// Example 1
+// let f;
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+// g();
+// f();
+// console.dir(f);
+
+// // reassigned f function
+// h();
+// f();
+// console.dir(f);
+
+// // Example 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`we are now boarding all ${n} passengers`);
+//     console.log(`there are 3 groups each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`will start boarding in ${wait} seconds`);
+// };
+
+// const perGroup = 1000;
+// boardPassengers(180, 3);
 
 // video 135 ============== Closure =======================
 
-const secureBooking = function() {
-  let passengerCount = 0;
+// const secureBooking = function() {
+//   let passengerCount = 0;
 
-  return function(){
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
-  }
-}
+//   return function(){
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   }
+// }
 
-const booker = secureBooking();
-booker();
-booker();
-booker();
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
 
-console.dir(booker)
+// console.dir(booker)
 
 // video 134 ==================== IIFE =====================
 
