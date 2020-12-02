3c0d61169dd70ded6ba182ca0f248900b5a6d01b
Grant Emerson <grant.alex.emerson@gmail.com>
2020-12-01 20:54:32 -0600

    section 11 coding challenge 1

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -61,24 +61,43 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ''; // similar to textContent returns only content innerHTML returns everything
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-// let arr = ['a', 'b', 'c', 'd', 'e'];
+let arr = ['a', 'b', 'c', 'd', 'e'];
 
-// // SLICE
-// console.log(arr.slice(2))
+// SLICE
+console.log(arr.slice(2))
 // console.log(arr.slice(2,4));
 // console.log(arr.slice(-2));
 // console.log(arr.slice(-1));
@@ -108,26 +127,91 @@ const currencies = new Map([
 // // JOIN
 // console.log(letters.join(' - '));
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// for (const movement of movements) {
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1} You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
-  }
-}
-console.log('---- FOREACH ----');
-movements.forEach(function (mov, i, arr) {
-  // current el, index, array this order is specific
-  if (mov > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${mov}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
-  }
-});
+// // for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1} You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+// console.log('---- FOREACH ----');
+// movements.forEach(function (mov, i, arr) {
+//   // current el, index, array this order is specific
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
 
 // 0: function(200)
 // 1: function (450)
 // etc ....
+
+// MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// SET does not have keys or index values
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+
+
+// Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy.
+// A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+// Your tasks:
+// Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+// 1. Juliafoundoutthattheownersofthefirstandthelasttwodogsactuallyhave cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+// 2. CreateanarraywithbothJulia's(corrected)andKate'sdata
+// 3. Foreachremainingdog,logtotheconsolewhetherit'sanadult("Dognumber1
+// is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy   ")
+// 4. Runthefunctionforbothtestdatasets
+// Test data:
+// § Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3] § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+// Hints: Use tools from all lectures in this section so far 😉
+
+// 1. 
+const checkDogs = function(arr1, arr2) {
+  arr1.shift();
+  arr1.splice(-2);
+  const newArr = (arr1.concat(arr2));
+  console.log(newArr);
+  newArr.forEach(function (value, key, map) {
+    // console.log(`${value}, ${key}`);
+    if (value > 3) {
+      console.log(`Dog number ${key} is and adult, and is ${value} years old`);
+    } else {
+      console.log(`Dog number ${key} is still a puppy`);
+    }
+  })
+}
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3]
+checkDogs(dogsJulia, dogsKate);
+
+const dogsJulia1 = [9, 16, 6, 8, 3];
+const dogsKate1 = [10, 5, 6, 1, 4]
+checkDogs(dogsJulia1, dogsKate1)
+
+// console.log(dogsJulia.shift());
+// console.log(dogsJulia.splice(-2));
+// console.log(dogsJulia);
+// const fullDogList = dogsKate.concat(dogsJulia);
+
+// 2. 
+// console.log(fullDogList);
+
+// 3. 
