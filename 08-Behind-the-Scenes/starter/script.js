3241c3eaf3170f4821f1821900b43c2ddb8605f6
Grant Emerson <grant.alex.emerson@gmail.com>
2020-12-16 19:49:55 -0600

    js course jumping back into it :)

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -101,7 +101,7 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
+      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, deposit) => acc + deposit, 0);
@@ -139,7 +139,7 @@ const CalcAvgAge = ages =>
     }, 0);
 const avg1 = CalcAvgAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = CalcAvgAge([16, 6, 10, 5, 6, 1, 4]);
-console.log(avg1, avg2);
+// console.log(avg1, avg2);
 
 // const CalcAvgAge = (ages) => ages
 //       .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
@@ -393,11 +393,24 @@ const max = movements.reduce((acc, mov) => {
 // CalcAvgAge([5, 2, 4, 1, 15, 8, 3]);
 
 const eurToUsd = 1.1;
-console.log(movements);
+// console.log(movements);
 
 // PIPELINE
 const totalDepositsUSD = movements
   .filter(mov => mov > 0)
   .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
-console.log(totalDepositsUSD);
+// console.log(totalDepositsUSD);
+
+
+const firstWithdrawal = movements.find(mov => mov < 0)
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+const account = accounts.find(ask => ask.owner === "Jessica Davis")
+console.log(account);
+
+let akk = []
+for (const mov of accounts) akk.push(mov.owner === "Sarah Smith");
+console.log(akk);
